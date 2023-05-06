baf1297d146609ad7e1b969e274afa1427f07877
RCJacH <RCJacH@outlook.com>
2023-05-07 04:35:50 +0800

    add basic commandID constant

---
+++ src/lib/constants.ts
@@ -0,0 +1,29 @@
+export const commandID = {
+  rewind: {
+    beat: 41045,
+    bar: 41043,
+    marker: 40172,
+  },
+  fforward: {
+    beat: 41044,
+    bar: 41042,
+    marker: 40173,
+  },
+  toggle: {
+    loop: 1068,
+    preroll: 41819,
+  },
+  transport: {
+    play: 1007,
+    record: 1013,
+    stop: 40667,
+    abort: 40668,
+  },
+  file: {
+    save: 40026,
+  },
+  edit: {
+    undo: 40029,
+    redo: 40030,
+  }
+}
