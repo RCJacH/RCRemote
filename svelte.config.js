2aa0f54950436d1811eeb6783c5795c76ae80ffd
RCJacH <RCJacH@outlook.com>
2023-05-07 04:27:16 +0800

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,6 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: [
-    vitePreprocess(),
-    sveltePreprocess(),
+    sveltePreprocess({ postcss: true }),
   ],
 }
