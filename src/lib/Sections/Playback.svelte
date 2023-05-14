e876a3fb1f703b4699c235c8e6b9daf17ccbe502
RCJacH <RCJacH@outlook.com>
2023-05-14 17:13:14 +0800

    fix layout in #playback

--- src/lib/Sections/Playback.svelte
+++ src/lib/Sections/Playback.svelte
@@ -5,20 +5,21 @@
 
   export let playstate = -1;
   let undostate = 0;
+  let allowPause = false;
 </script>
 
 <template lang="pug">
   #playback
-    +if('playstate <= 0')
+    +if('playstate <= 0 || !allowPause')
       Button(visual="{visual_style}", name="play")
-    +if('playstate > 0')
-      Button(visual="{visual_style}", name="pause")
+      +elseif('playstate > 0 && allowPause')
+        Button(visual="{visual_style}", name="pause")
     Button(visual="{visual_style}", name="record")
     +if('playstate > 0')
       Button(visual="{visual_style}", name="stop")
       +else()
         Button(visual="{visual_style}", name="save")
-    +if('undostate == -1')
+    +if('playstate&3')
       Button(visual="{visual_style}", name="abort")
       +elseif('undostate == 1')
         Button(visual="{visual_style}", name="redo")
@@ -31,10 +32,16 @@
     display: flex;
     flex-wrap: wrap;
     flex: 6;
-    flex-direction: row;
-    :global(.c-button) {
-      max-width: 50%;
-      min-width: 35%;
+    justify-content: stretch;
+    align-items: stretch;
+
+    :global(& .c-button) {
+      flex-basis: 50%;
+      height: 50%;
+
+      @media screen and (orientation: landscape) {
+        flex-basis: 25%;
+      }
     }
   }
 </style>
