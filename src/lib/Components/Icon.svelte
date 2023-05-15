2f1ccd8ad350800cca12052d5b82e15268c1032e
RCJacH <RCJacH@outlook.com>
2023-05-15 19:34:00 +0800

    add more icons for #playback

--- src/lib/Components/Icon.svelte
+++ src/lib/Components/Icon.svelte
@@ -1,9 +1,11 @@
 <!-- Icon.svelte -->
 <script>
+  import Save from "./icons/Save.svelte";
   import Abort from "./icons/Abort.svelte";
   import Pause from "./icons/Pause.svelte";
   import Play from "./icons/Play.svelte";
   import Record from "./icons/Record.svelte";
+  import Redo from "./icons/Redo.svelte";
   import Stop from "./icons/Stop.svelte";
   import Undo from "./icons/Undo.svelte";
 </script>
@@ -15,6 +17,8 @@
       Pause
       Play
       Record
+      Redo
+      Save
       Stop
       Undo
 </template>
