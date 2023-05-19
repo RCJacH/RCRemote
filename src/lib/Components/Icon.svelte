2d970c23402de0ec39348ea7c5800981dc90b1d9
RCJacH <RCJacH@outlook.com>
2023-05-20 02:11:05 +0800

    add more icons

--- src/lib/Components/Icon.svelte
+++ src/lib/Components/Icon.svelte
@@ -1,13 +1,17 @@
 <!-- Icon.svelte -->
 <script>
-  import Rewind from "./icons/Rewind.svelte";
-  import FForward from "./icons/FForward.svelte";
-  import Save from "./icons/Save.svelte";
   import Abort from "./icons/Abort.svelte";
+  import Beat from "./icons/Beat.svelte";
+  import FForward from "./icons/FForward.svelte";
+  import Loop from "./icons/Loop.svelte";
+  import Measure from "./icons/Measure.svelte";
+  import Menu from "./icons/Menu.svelte";
   import Pause from "./icons/Pause.svelte";
   import Play from "./icons/Play.svelte";
   import Record from "./icons/Record.svelte";
   import Redo from "./icons/Redo.svelte";
+  import Rewind from "./icons/Rewind.svelte";
+  import Save from "./icons/Save.svelte";
   import Stop from "./icons/Stop.svelte";
   import Undo from "./icons/Undo.svelte";
 </script>
@@ -17,6 +21,9 @@
     defs
       Abort
       FForward
+      Loop
+      Measure
+      Menu
       Pause
       Play
       Record
