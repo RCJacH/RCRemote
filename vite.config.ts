322f5a76324996543a7dd3dc856549218b5b6469
RCJacH <RCJacH@outlook.com>
2023-05-23 02:36:26 +0800

    update config to build single file rcremote.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
+/**
+ * @param newFilename {string}
+ * @returns {import ('vite').Plugin}
+ */
+const renameIndexPlugin = (newFilename) => {
+  if (!newFilename) return
+  return {
+    name: 'renameIndex',
+    enforce: 'post',
+    generateBundle(options, bundle) {
+      const indexHtml = bundle['index.html']
+      indexHtml.fileName = newFilename
+    },
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    viteSingleFile(),
+    renameIndexPlugin('rcremote.html')
+  ]
 })
