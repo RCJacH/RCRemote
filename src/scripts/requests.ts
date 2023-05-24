b32c6e87932c86319fce9d8499323825de3a6d64
RCJacH <RCJacH@outlook.com>
2023-05-24 19:39:41 +0800

    refactor requests.ts

--- src/scripts/requests.ts
+++ src/scripts/requests.ts
@@ -1,13 +1,15 @@
 import { writable } from 'svelte/store';
 import { wwr_onreply } from './onreply';
 
+type XmlHttpRequestOrFalse = XMLHttpRequest | false;
+
 let freq: number = 100;
 let once: string = '';
 let recur: Array<[string, number, number]> = [];
 const response = createResponse();
-let req: XMLHttpRequest = initxmlhttp();
-let timer: ReturnType<typeof setTimeout> = null;
-let timer2: ReturnType<typeof setTimeout> = null;
+let req: XmlHttpRequestOrFalse = initxmlhttp();
+let timer: ReturnType<typeof setTimeout> | null = null;
+let timer2: ReturnType<typeof setTimeout> | null = null;
 let errcnt: number = 0;
 
 function createResponse() {
@@ -18,75 +20,87 @@ function createResponse() {
   }
 }
 
-function initxmlhttp() {
-  let xmlhttp = null;
-  if (!xmlhttp && typeof XMLHttpRequest != 'undefined') {
-    try { xmlhttp = new XMLHttpRequest(); } catch (e) { xmlhttp = null; }
+function initxmlhttp(): XmlHttpRequestOrFalse {
+  let xmlhttp: XmlHttpRequestOrFalse = false;
+  if (typeof XMLHttpRequest !== 'undefined') {
+    try {
+      xmlhttp = new XMLHttpRequest();
+    } catch (e) {
+      xmlhttp = false;
+    }
   }
+
   if (!xmlhttp && window.createRequest) {
-    try { xmlhttp = window.createRequest(); } catch (e) { xmlhttp = null; }
+    try {
+      xmlhttp = window.createRequest();
+    } catch (e) {
+      xmlhttp = false;
+    }
   }
+
   return xmlhttp;
 }
 
-function addCommand(name: string | number) {
-  once += name + ";";
+function addCommand(name: string | number): void {
+  once += `${name};`;
 }
 
-function addRecur(name: string, interval: number) {
+function addRecur(name: string, interval: number): void {
   recur.push([name, interval, 0]);
 }
 
-function removeRecur(name: string) {
-  for (let i = 0; i < recur.length; ++i) {
-    if (recur[i] && recur[i][0] === name) {
-      recur.splice(i, 1);
-      break;
-    }
-  }
+function removeRecur(name: string): void {
+  recur = recur.filter((item) => { item[0] !== name })
 }
 
 function concatRecur(): string {
-  let s = ""
-  let d = (new Date).getTime();
-  for (var i = 0; i < recur.length; i++) {
-    let x = recur[i];
-    if (x[2] < d) {
-      x[2] = d + x[1];
-      s += x[0] + ";";
+  const now = Date.now();
+  return recur.reduce((acc, item, index) => {
+    if (item[2] < now) {
+      item[2] = now + item[1];
+      return acc + `${item[0]};`;
     }
+    return acc;
+  }, "");
+}
+
+function pendingUpdate(): void {
+  if (errcnt > 2) {
+    timer = setTimeout(update, 100 << (errcnt - 3));
+  } else {
+    update();
   }
-  return s;
 }
 
-function pendingUpdate() {
-  if (errcnt > 2) timer = setTimeout(
-    () => { update },
-    100 << (errcnt - 3)
-  );
-  else update();
-}
-
-function onreadystatechange() {
-  if (req.readyState == 4) {
-    if (timer2) { clearTimeout(timer2); timer2 = null; }
-    if (req.responseText != "") {
+function onreadystatechange(): void {
+  if (req && req.readyState === 4) {
+    if (timer2) {
+      clearTimeout(timer2);
+      timer2 = null;
+    }
+    if (req.responseText !== "") {
       errcnt = 0;
       response.set(wwr_onreply(req.responseText));
-    } else if (req.getResponseHeader("Server") == null) {
+    } else if (req.getResponseHeader("Server") === null) {
       if (errcnt < 8) errcnt++;
+
+      req = initxmlhttp();
     }
     pendingUpdate();
   }
 };
 
-function onTimeout() {
+function onTimeout(): void {
   timer2 = null;
-  if (req.readyState != 0 && req.readyState != 4) {
-    if (timer) { clearTimeout(timer); timer = null; }
+  if (req && req.readyState != 0 && req.readyState != 4) {
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+    }
     req.abort();
     if (errcnt < 8) errcnt++;
 
+    req = initxmlhttp();
     pendingUpdate();
   }
 }
@@ -97,18 +111,18 @@ function update(): void {
   if (!req) { alert("no xml http support"); return; }
 
   let requestString = once + concatRecur();
-  if (requestString == "") {
-    timer = setTimeout(() => { update }, freq);
+  if (requestString === "") {
+    timer = setTimeout(update, freq);
     return;
   }
 
-  req.open("GET", "/_/" + requestString, true);
-  req.onreadystatechange = () => { onreadystatechange() };
+  req.open("GET", `/_/${requestString}`, true);
+  req.onreadystatechange = onreadystatechange;
   if (timer2) clearTimeout(timer2);
 
-  timer2 = setTimeout(() => { onTimeout }, 3000);
+  timer2 = setTimeout(onTimeout, 3000);
   once = '';
-  req.send(null);
+  req.send();
 }
 
 export { addCommand, addRecur, removeRecur, update, response };
