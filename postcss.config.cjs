161ddbf6db8d014601c777c9c4001331dc93df17
RCJacH <RCJacH@outlook.com>
2023-05-28 23:45:06 +0800

    update postcss.config.cjs to recommended style

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
 
-const config = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-initial': {},
-    'postcss-simple-vars': {},
-    'postcss-nested': {},
-    'postcss-preset-env': {},
-    'autoprefixer': {},
-  }
-}
-
-module.exports = config
+module.exports = (ctx) => ({
+  plugins: [
+    require('postcss-import'),
+    require('postcss-initial'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('postcss-preset-env'),
+    require('autoprefixer'),
+  ]
+})
