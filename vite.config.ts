d83626ad3b8e1fe58904a15cc83f046214ca390d
RCJacH <RCJacH@outlook.com>
2023-06-02 14:11:15 +0800

    rename output file to Titlecase

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig(({ mode }) => {
       pugPlugin({}),
       isDev ? mockServer : [],
       viteSingleFile(),
-      renameIndexPlugin("rcremote.html")
+      renameIndexPlugin("RCRemote.html")
     ],
     resolve: {
       alias: {
